be5a3147c79201f0de093187a7b68b93fa77bbff
Sergey <graygmai1@gmail.com>
2022-05-29 06:25:40 +0500

    Сделал ещё 1 секцию, там же  сам  реализовал простой скрипт js

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,14 @@
 const hamburger = document.querySelector('.promo__hamburger'),
     menu = document.querySelector('.menu'),
-    menuClose = document.querySelector('.menu__close');
+    menuClose = document.querySelector('.menu__close'),
+    skillLine = document.querySelectorAll('.skills__percent-wrap_scale span');
 
+// *SKILL LINE HEIGHT* //
+skillLine.forEach(element => {
+    element.style.width = `${element.parentElement.previousElementSibling.firstElementChild.innerText}`;
+});
 
-// console.log(hamburger);
-// console.log(menu);
-
+// *MENU* //
 hamburger.addEventListener('click', function() {
     menu.classList.toggle('menu_active');
     hamburger.classList.toggle('hamburger_active');
