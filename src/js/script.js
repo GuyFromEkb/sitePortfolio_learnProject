9bc4b16b13e9c043bdf7e38255aacc176f6c08c9
Sergey <graygmai1@gmail.com>
2022-05-30 06:44:44 +0500

    Сверстал 2 секции

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,15 @@
-const hamburger = document.querySelector('.promo__hamburger'),
-    menu = document.querySelector('.menu'),
-    menuClose = document.querySelector('.menu__close'),
-    skillLine = document.querySelectorAll('.skills__percent-wrap_scale span');
-
 // *SKILL LINE HEIGHT* //
+const skillLine = document.querySelectorAll('.skills__percent-wrap_scale span');
+
 skillLine.forEach(element => {
     element.style.width = `${element.parentElement.previousElementSibling.firstElementChild.innerText}`;
 });
 
 // *MENU* //
+const hamburger = document.querySelector('.promo__hamburger'),
+    menu = document.querySelector('.menu'),
+    menuClose = document.querySelector('.menu__close');
+
 hamburger.addEventListener('click', function() {
     menu.classList.toggle('menu_active');
     hamburger.classList.toggle('hamburger_active');
