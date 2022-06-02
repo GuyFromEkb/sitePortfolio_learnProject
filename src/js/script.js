fb3890e690453a7190dcf10864ad46214eb8ba21
Sergey <graygmai1@gmail.com>
2022-06-02 06:57:39 +0500

    скрипт на плавный скрол на js подправил ссылки, довел все до ума, как мне кажется

--- src/js/script.js
+++ src/js/script.js
@@ -8,6 +8,7 @@ skillLine.forEach(element => {
 // *MENU* //
 const hamburger = document.querySelector('.promo__hamburger'),
     menu = document.querySelector('.menu'),
+    menuLink = document.querySelectorAll('.menu__link'),
     menuClose = document.querySelector('.menu__close');
 
 hamburger.addEventListener('click', function() {
@@ -17,5 +18,24 @@ hamburger.addEventListener('click', function() {
 menuClose.addEventListener('click', function() {
     menu.classList.toggle('menu_active');
     hamburger.classList.toggle('hamburger_active');
-
 });
+menuLink.forEach(item => {
+    item.addEventListener('click', () => {
+        menu.classList.toggle('menu_active');
+        hamburger.classList.toggle('hamburger_active');
+    });
+});
+
+// smooth scroll
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function(e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
