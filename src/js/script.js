9de01bdc848cc21a9c1c1e49285a21ddee553803
Sergey <graygmai1@gmail.com>
2022-06-02 07:10:13 +0500

    add DOMContentLoaded

--- src/js/script.js
+++ src/js/script.js
@@ -1,41 +1,47 @@
-// *SKILL LINE HEIGHT* //
-const skillLine = document.querySelectorAll('.skills__percent-wrap_scale span');
+"use strict";
+window.addEventListener('DOMContentLoaded', () => {
 
-skillLine.forEach(element => {
-    element.style.width = `${element.parentElement.previousElementSibling.firstElementChild.innerText}`;
-});
 
-// *MENU* //
-const hamburger = document.querySelector('.promo__hamburger'),
-    menu = document.querySelector('.menu'),
-    menuLink = document.querySelectorAll('.menu__link'),
-    menuClose = document.querySelector('.menu__close');
+    // *SKILL LINE HEIGHT* //
+    const skillLine = document.querySelectorAll('.skills__percent-wrap_scale span');
 
-hamburger.addEventListener('click', function() {
-    menu.classList.toggle('menu_active');
-    hamburger.classList.toggle('hamburger_active');
-});
-menuClose.addEventListener('click', function() {
-    menu.classList.toggle('menu_active');
-    hamburger.classList.toggle('hamburger_active');
-});
-menuLink.forEach(item => {
-    item.addEventListener('click', () => {
+    skillLine.forEach(element => {
+        element.style.width = `${element.parentElement.previousElementSibling.firstElementChild.innerText}`;
+    });
+
+    // *MENU* //
+    const hamburger = document.querySelector('.promo__hamburger'),
+        menu = document.querySelector('.menu'),
+        menuLink = document.querySelectorAll('.menu__link'),
+        menuClose = document.querySelector('.menu__close');
+
+    hamburger.addEventListener('click', function() {
         menu.classList.toggle('menu_active');
         hamburger.classList.toggle('hamburger_active');
     });
-});
-
-// smooth scroll
-const smoothLinks = document.querySelectorAll('a[href^="#"]');
-for (let smoothLink of smoothLinks) {
-    smoothLink.addEventListener('click', function(e) {
-        e.preventDefault();
-        const id = smoothLink.getAttribute('href');
-
-        document.querySelector(id).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
+    menuClose.addEventListener('click', function() {
+        menu.classList.toggle('menu_active');
+        hamburger.classList.toggle('hamburger_active');
+    });
+    menuLink.forEach(item => {
+        item.addEventListener('click', () => {
+            menu.classList.toggle('menu_active');
+            hamburger.classList.toggle('hamburger_active');
         });
     });
-};
+
+    // smooth scroll
+    const smoothLinks = document.querySelectorAll('a[href^="#"]');
+    for (let smoothLink of smoothLinks) {
+        smoothLink.addEventListener('click', function(e) {
+            e.preventDefault();
+            const id = smoothLink.getAttribute('href');
+
+            document.querySelector(id).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    };
+
+});
